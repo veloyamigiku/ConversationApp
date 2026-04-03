7d77e750921a630ef6c06c3486bedb942d799ef1
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2026-04-03 22:10:49 +0900

    ・新規作成(ConversationProto)。

---
+++ ConversationProto/conversationproto/app.py
@@ -0,0 +1,12 @@
+import json
+
+def lambda_handler(event, context):
+  print('exec.')
+  """
+  return {
+    "statusCode": 200,
+    "body": json.dumps({
+      "message": "hello world",
+    }),
+  }
+  """
