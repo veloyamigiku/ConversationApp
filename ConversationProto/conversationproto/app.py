b40f05a20babc34444a72a402ec0d37c9bc75272
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2026-05-01 18:20:52 +0900

    ・会話のLambda関数を修正する。 　DynamoDBのテーブルへの読み書きをサポートする。

--- ConversationProto/conversationproto/app.py
+++ ConversationProto/conversationproto/app.py
@@ -1,12 +1,50 @@
-import json
+import boto3
+from datetime import datetime
+from datetime import timezone
+import time
+
+dynamodb = boto3.resource('dynamodb')
+conversation_table = dynamodb.Table('ConversationProto')
 
 def lambda_handler(event, context):
   print('exec.')
-  """
-  return {
-    "statusCode": 200,
-    "body": json.dumps({
-      "message": "hello world",
-    }),
+
+  now = datetime.now(timezone.utc)
+  iso_date = now.isoformat()
+  timestamp = int(time.time())
+
+  body_data = event.get('body', {})
+  print('type(body_data): {}'.format(type(body_data)))
+  print('body_data: {}'.format(body_data))
+  conversation_id = body_data.get('conversation_id')
+  comment = body_data.get('comment')
+
+  item = {
+    'conversation_id': conversation_id,
+    'comment': comment,
+    'created_at': iso_date,
+    'timestamp': timestamp
   }
-  """
+  print('item: {}'.format(item))
+
+  try:
+    conversation_table.put_item(Item=item)
+  except Exception as e:
+    print('===error: {}'.format(e))
+
+if __name__ == '__main__':
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--conversation_id', type=str, default=None)
+  parser.add_argument('--comment', type=str, default=None)
+  args = parser.parse_args()
+  body = {
+    'conversation_id': args.conversation_id,
+    'comment': args.comment
+  }
+  event = {
+    'body': body
+  }
+  lambda_handler(
+    event=event,
+    context=None)
